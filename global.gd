ad290ec731749f67979e766887dbd408a71fad12
Eibriel <eibriel@eibriel.com>
2024-04-26 00:08:40 -0300

    Detecting Demo build

--- global.gd
+++ global.gd
@@ -4,3 +4,15 @@ var character: Node3D
 var camera: Camera3D
 var camera_path: Path3D
 var icon: TextureRect
+
+func is_demo():
+	var is_demo:bool = OS.has_feature("demo")
+	if not is_demo:
+		if not FileAccess.file_exists("res://not_demo.gd"):
+			quit_game()
+	return is_demo
+
+func quit_game():
+	# See https://docs.godotengine.org/en/stable/tutorials/inputs/handling_quit_requests.html
+	# get_tree().root.propagate_notification(NOTIFICATION_WM_CLOSE_REQUEST)
+	get_tree().quit()
--- main.gd
+++ main.gd
@@ -1,6 +1,11 @@
 extends Control
 
 @onready var dk_world: DKWorld = %DKWorld
+@onready var label_demo: Label = $Control/LabelDemo
 
 func _ready() -> void:
 	Global.icon = $Control/Icon
+
+	label_demo.visible = false
+	if Global.is_demo():
+		label_demo.visible = true
